7564cd09063dfe50fe9bcd6a1b3fd2cb66dd6b41
Vladislav T <vt118@mail.ru>
2020-06-18 20:44:52 +0300

    feat: add left sideNav

--- src/js/plugins/initMaterial.js
+++ src/js/plugins/initMaterial.js
@@ -1,1 +1,14 @@
+/* eslint-disable */
+import 'materialize-css';
+
 // init Materialize components
+const initSideNavs = () => {
+  const elems = document.querySelectorAll('.sidenav');
+  const instances = M.Sidenav.init(elems);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  initSideNavs ();
+});
+
+
