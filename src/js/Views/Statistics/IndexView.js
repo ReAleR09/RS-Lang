8b3df87f03da3c1d3dccff826fa0fcb9ef58ff7c
Bogdan Z <realer09@gmail.com>
2020-07-09 22:29:47 +0300

    fix: enable preloader

--- src/js/Views/Statistics/IndexView.js
+++ src/js/Views/Statistics/IndexView.js
@@ -1,24 +1,23 @@
 import Materialize from 'materialize-css';
 import View from '../../lib/View';
 import { STATISTICS_HTML } from '../../Components/Statistics/Templates';
-// import {
-//   showPreloader,
-//   hidePreloader,
-// } from '../../Classes/Preloader';
+import {
+  showPreloader,
+  hidePreloader,
+} from '../../Classes/Preloader';
 import StatisticsPageManager from '../../Components/Statistics/StatisticsPageManager';
 
 export default class IndexView extends View {
   // eslint-disable-next-line class-methods-use-this
   async onMount() {
-    // showPreloader();
+    showPreloader();
     // init tabs
     const mainTabsEl = this.element.querySelectorAll('.tabs');
     Materialize.Tabs.init(mainTabsEl);
 
     const pageManager = new StatisticsPageManager(this.element);
     await pageManager.drawStatistics();
-    console.log('kek');
-    // hidePreloader();
+    hidePreloader();
   }
 
   // eslint-disable-next-line class-methods-use-this
