2767c621f1f3e4ca18cdcf7a3c226a51e7c706f1
Yauheni Yafimenka <yafimenka.jk@gmail.com>
2020-07-10 22:29:47 +0300

    fix: fix navibar structure

--- src/config.js
+++ src/config.js
@@ -57,11 +57,11 @@ export const SIDENAV = [
         path: ['savannah'],
         icon: 'extension',
       },
+      {
+        title: 'Field of Dreams [mini-game]',
+        path: ['fieldOfDreams'],
+        icon: 'slow_motion_video',
+      },
     ],
-  },
-  {
-    title: 'Field of Dreams [mini-game]',
-    path: ['fieldOfDreams'],
-    icon: 'slow_motion_video',
   },
 ];
